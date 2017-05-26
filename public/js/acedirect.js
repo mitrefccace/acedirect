f2f0db29a9444e3b759e271d370ffd0cb34e181c
Mike Woodman <mwoodman@mitre.org>
2017-05-26 12:58:05 -0400

    Controls to copy light code to clipboard

--- public/js/acedirect.js
+++ public/js/acedirect.js
@@ -76,6 +76,9 @@ function connect_socket() {
 					$('#agentname-headerdropdown').html(payload.first_name + " " + payload.last_name);
 					$('#agentrole-headerdropdown').html("<small>" + payload.role + "</small>");
 					$('#agentlightcode-headerdropdown').html("<small>Light Code: " + payload.lightcode + "</small>");
+					var lighturl = "https://" + window.location.hostname + window.location.pathname	+"/"+payload.lightcode;				
+					lighturl = lighturl.replace('/agent/','/getagentstatus/');
+					$('#agentlightcode').val(lighturl);
 
 					if (payload.queue_name === "ComplaintsQueue" || payload.queue2_name === "ComplaintsQueue") {
 						$('#sidebar-complaints').show();
@@ -519,6 +522,64 @@ function clearScreen() {
 	$('#notickettxt').hide();
 	$('#ticketTab').removeClass("bg-pink");
 	clearInterval(ticketTabFade);
+}
+
+document.getElementById("copyLightcodeButton").addEventListener("click", function() {
+    copyToClipboard(document.getElementById("agentlightcode"));
+	var x = document.getElementById("snackbar")
+	x.innerHTML = "Light Code Copied to Clipboard.";
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+});
+
+function copyToClipboard(elem) {
+	  // create hidden text element, if it doesn't already exist
+    var targetId = "_hiddenCopyText_";
+    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+    var origSelectionStart, origSelectionEnd;
+    if (isInput) {
+        // can just use the original source element for the selection and copy
+        target = elem;
+        origSelectionStart = elem.selectionStart;
+        origSelectionEnd = elem.selectionEnd;
+    } else {
+        // must use a temporary form element for the selection and copy
+        target = document.getElementById(targetId);
+        if (!target) {
+            var target = document.createElement("textarea");
+            target.style.position = "absolute";
+            target.style.left = "-9999px";
+            target.style.top = "0";
+            target.id = targetId;
+            document.body.appendChild(target);
+        }
+        target.textContent = elem.textContent;
+    }
+    // select the content
+    var currentFocus = document.activeElement;
+    target.focus();
+    target.setSelectionRange(0, target.value.length);
+    
+    // copy the selection
+    var succeed;
+    try {
+    	  succeed = document.execCommand("copy");
+    } catch(e) {
+        succeed = false;
+    }
+    // restore original focus
+    if (currentFocus && typeof currentFocus.focus === "function") {
+        currentFocus.focus();
+    }
+    
+    if (isInput) {
+        // restore prior selection
+        elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+    } else {
+        // clear temporary content
+        target.textContent = "";
+    }
+    return succeed;
 }
 
 // Debug Functions for sidebar.
