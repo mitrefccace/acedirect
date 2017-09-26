130856db17068658e88ec79a69b0b0a1a0539613
Mike Woodman <mwoodman@mitre.org>
2017-09-26 10:30:28 -0400

    Removed busylight input field

--- public/js/acedirect.js
+++ public/js/acedirect.js
@@ -112,14 +112,10 @@ function connect_socket() {
 					$('#agentname-header').html(payload.first_name + " " + payload.last_name);
 					$('#agentname-headerdropdown').html(payload.first_name + " " + payload.last_name);
 					$('#agentrole-headerdropdown').html("<small>" + payload.role + "</small>");
-					$('#agentlightcode-headerdropdown').html("<small>Light Code: " + payload.lightcode + "</small>");
 					$('#ws_servers').attr("name","wss://" + payload.asteriskPublicHostname + "/ws");
 					$('#my_sip_uri').attr("name","sip:"+payload.extension+"@"+payload.asteriskPublicHostname);
 					$('#sip_password').attr("name",payload.extensionPassword);
-					$("#pc_config").attr("name","stun:" + payload.stunServer );																																																	 
-					var lighturl = "https://" + window.location.hostname + window.location.pathname + "/" + payload.lightcode;
-					lighturl = lighturl.replace('/agent/', '/getagentstatus/');
-					$('#agentlightcode').val(lighturl);
+					$("#pc_config").attr("name","stun:" + payload.stunServer );				
 
 					if (payload.queue_name === "ComplaintsQueue" || payload.queue2_name === "ComplaintsQueue") {
 						$('#sidebar-complaints').show();
@@ -676,68 +672,6 @@ function clearScreen() {
 function changeStatusLight(state) {
 	this.agentStatus = state;
 	busylight.light(state);
-}
-
-
-
-document.getElementById("copyLightcodeButton").addEventListener("click", function () {
-	copyToClipboard(document.getElementById("agentlightcode"));
-	var x = document.getElementById("snackbar")
-	x.innerHTML = "Light Code Copied to Clipboard.";
-	x.className = "show";
-	setTimeout(function () {
-		x.className = x.className.replace("show", "");
-	}, 3000);
-});
-
-function copyToClipboard(elem) {
-	// create hidden text element, if it doesn't already exist
-	var targetId = "_hiddenCopyText_";
-	var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
-	var origSelectionStart, origSelectionEnd;
-	if (isInput) {
-		// can just use the original source element for the selection and copy
-		target = elem;
-		origSelectionStart = elem.selectionStart;
-		origSelectionEnd = elem.selectionEnd;
-	} else {
-		// must use a temporary form element for the selection and copy
-		target = document.getElementById(targetId);
-		if (!target) {
-			var target = document.createElement("textarea");
-			target.style.position = "absolute";
-			target.style.left = "-9999px";
-			target.style.top = "0";
-			target.id = targetId;
-			document.body.appendChild(target);
-		}
-		target.textContent = elem.textContent;
-	}
-	// select the content
-	var currentFocus = document.activeElement;
-	target.focus();
-	target.setSelectionRange(0, target.value.length);
-
-	// copy the selection
-	var succeed;
-	try {
-		succeed = document.execCommand("copy");
-	} catch (e) {
-		succeed = false;
-	}
-	// restore original focus
-	if (currentFocus && typeof currentFocus.focus === "function") {
-		currentFocus.focus();
-	}
-
-	if (isInput) {
-		// restore prior selection
-		elem.setSelectionRange(origSelectionStart, origSelectionEnd);
-	} else {
-		// clear temporary content
-		target.textContent = "";
-	}
-	return succeed;
 }
 
 // Debug Functions for sidebar.
