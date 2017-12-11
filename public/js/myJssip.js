034e8260e261455b3d7938d13dbe5e7f0560812f
Mike Woodman <mwoodman@mitre.org>
2017-12-11 15:11:03 -0500

    newmessage event for recording

--- public/js/myJssip.js
+++ public/js/myJssip.js
@@ -120,6 +120,9 @@ function register_jssip()
 	});
 	ua.on('newMessage', function(e){
 		if(debug) console.log("\nUA - NEWMESSAGE");
+
+		if (complaintForm && e.content == 'STARTRECORDING')
+        	startRecordProgress(); 
 	});
 	ua.on('newRTCSession', function(e){
 		//e.request.body = edit_request(e.request.body);
@@ -178,9 +181,7 @@ function register_jssip()
 	    currentSession.on('newInfo', function(e)
 		{
 			if(debug) console.log('\nCURRENTSESSION -  NEWINFO: \nINFO:\n' + e.info + "\nrequest:\n" + e.request);
-      if (complaintForm)
-        startRecordProgress(); //newInfo gets called repeatedly during a call
-		});
+      
 		currentSession.on('hold', function(e)
 		{
 			if(debug) console.log('\nCURRENTSESSION -  HOLD ');
